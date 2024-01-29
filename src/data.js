cdece710613a7abf3b7a21dc22ac0d94ded08e21
Kirk Vander Kuy <kvk807@gmail.com>
2024-01-28 21:47:06 -0600

    first commit

---
+++ src/data.js
@@ -0,0 +1,93 @@
+export const pageLinks = [
+  { id: 1, href: '#home', text: 'home' },
+  { id: 2, href: '#about', text: 'about' },
+  { id: 3, href: '#services', text: 'services' },
+  { id: 4, href: '#tours', text: 'tours' },
+]
+export const socialLinks = [
+  { id: 1, href: 'https://www.facebook.com', icon: 'fab fa-facebook' },
+  { id: 2, href: 'https://www.twitter.com', icon: 'fab fa-twitter' },
+  { id: 3, href: 'https://www.squarespace.com', icon: 'fab fa-squarespace' },
+]
+export const services = [
+  {
+    id: 1,
+    icon: 'fas fa-wallet fa-fw',
+    title: 'saving money',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, molestiae.',
+  },
+  {
+    id: 2,
+    icon: 'fas fa-tree fa-fw',
+    title: 'endless hiking',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, molestiae.',
+  },
+  {
+    id: 3,
+    icon: 'fas fa-socks fa-fw',
+    title: 'amazing comfort',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, molestiae.',
+  },
+]
+export const tours = [
+  {
+    id: 1,
+    image: './assets/images/tour-1.jpeg',
+    date: 'august 26th, 2020',
+    title: 'Tibet Adventure',
+    info: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.`,
+    location: 'china',
+    duration: 6,
+    cost: 2100,
+  },
+  {
+    id: 2,
+    image: './assets/images/tour-2.jpeg',
+    date: 'october 1th, 2020',
+    title: 'best of java',
+    info: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.`,
+    location: 'indonesia',
+    duration: 11,
+    cost: 1400,
+  },
+  {
+    id: 3,
+    image: './assets/images/tour-3.jpeg',
+    date: 'september 15th, 2020',
+    title: 'explore hong kong',
+    info: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.`,
+    location: 'hong kong',
+    duration: 8,
+    cost: 5000,
+  },
+  {
+    id: 4,
+    image: './assets/images/tour-4.jpeg',
+    date: 'december 5th, 2019',
+    title: 'kenya highlights',
+    info: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.`,
+    location: 'kenya',
+    duration: 20,
+    cost: 3300,
+  },
+  {
+    id: 5,
+    image: './assets/images/tour-5.jpeg',
+    date: 'may 25, 2021',
+    title: 'kenya highlights',
+    info: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.`,
+    location: 'zimbabwe',
+    duration: 14,
+    cost: 2300,
+  },
+  {
+    id: 6,
+    image: './assets/images/tour-6.jpeg',
+    date: 'june 14, 2023',
+    title: 'see incredible india',
+    info: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.`,
+    location: 'india',
+    duration: 28,
+    cost: 4230,
+  },
+]
